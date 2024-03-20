730ba7fab8de52a3848351d545ddb6df5821f98f
Luka <blagicluka666@gmail.com>
2024-03-20 19:29:01 +0100

    Redesign Login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
-import { Routes, Route, useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { loginUser } from "../api/Auth";
 import { ToastContext } from "../utility/contexts/ToastContext";
 import { AuthContext } from "../utility/contexts/AuthContext";
 import { getUserData } from "../api/User";
 
-const Login = () => {
+export const Login = ({ }) => {
+
   const { showToast } = useContext(ToastContext);
   const { setAuth, setToken, setUser } = useContext(AuthContext);
   const [email, setEmail] = useState('');
@@ -30,53 +30,59 @@ const Login = () => {
       const token = await loginUser(email, password);
       setAuth(true);
       setToken(token);
-      localStorage.setItem("token",token);
+      sessionStorage.setItem("token", token);
       setUser(getUserData(token));
       const userData = await getUserData(token);
-      localStorage.setItem("user",JSON.stringify(userData));
-      showToast("success",'Login successful!');
+      sessionStorage.setItem("user", JSON.stringify(userData));
+      showToast("success", 'Login successful!');
       navigateToHome();
     } catch (error) {
       console.error(error);
-      showToast("success",'Login failed!');
+      showToast("success", 'Login failed!');
     }
   };
 
   return (
-      <Container className="login-wrapper">
-        <h2>Login</h2>
-        <Form onSubmit={handleLogin}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-            />
-          </Form.Group>
+    <div className="container w-64 my-auto">
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email address
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-            />
-          </Form.Group>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
 
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Login
+        </button>
+      </form>
 
-        <p>
-          Don't have an account?{" "}
-          <Link to="/register">Register</Link>
-        </p>
-      </Container>
+      <p className="mt-4">
+        Don't have an account?
+        <Link className="text-blue-500 hover:text-blue-800" to="/register">Register</Link>
+      </p>
+    </div>
   );
 };
-
-export default Login;
