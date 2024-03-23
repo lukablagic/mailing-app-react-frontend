9e400f4a461fae8c8f47f3604289fe94e61298d8
Luka <blagicluka666@gmail.com>
2024-03-24 00:50:36 +0100

    Final design

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -43,48 +43,60 @@ export const Login = ({ }) => {
   };
 
   return (
-    <div className="h-full w-full bg-teal-50 flex items-center justify-center">
-      <div className="flex flex-row justify-start items-center gap-4 w-full">
-        <div className=" w-6/12 h-2/3 m-16 p-12 border border-teal-100 rounded-xl">
-          <h2 className="text-2xl font-bold">Login</h2>
+    <div className="h-full w-full bg-teal-50 flex items-csenter justify-center">
+      <div className="flex flex-row h-max-h-screen w-full justify-start items-center gap-4  ">
+        <div className="w-8/12 items-left px-48 h-full flex flex-col grow-1 shrink-1 justify-center gap-4 ">
+          <h1 className="text-4xl font-bold text-sky-400">Dev Mail</h1>
+          <h1 className="text-gray-500">Welcome back, please login to your account.</h1>
+          <h2 className="text-xl font-bold  ">Login</h2>
           <p className=" my-2 text-black">
-            Create account  <Link className="text-blue-500 hover:text-blue-800" to="/register">Register</Link>
+            Don't have an account yet? <Link className="text-cyan-500 hover:text-cyan-800" to="/register">Register</Link>
           </p>
-          <form onSubmit={handleLogin}>
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          <form className="flex flex-col gap-3 " onSubmit={handleLogin}>
+            <label className="block text-gray-700 text-sm font-bold " htmlFor="email">
               Email address
             </label>
             <input
-              className="shadow  border appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow  border appearance-none rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="email"
               placeholder="Enter email"
               value={email}
               onChange={(event) => setEmail(event.target.value)}
             />
-            <div className="mb-6">
-              <label className="block text-sm font-bold mb-2  text-gray-700" htmlFor="password">
+            <div className="flex flex-row justify-between items-center">
+              <label className="block  font-bold  text-gray-700" htmlFor="password">
                 Password
               </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline text-gray-700"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-              />
+              <a className="text-cyan-500 hover:text-cyan-800 " href="#">
+                Forgot Password?
+              </a>
             </div>
-
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-              Login
-            </button>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-2  leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </form>
+          <div className="flex items-center justify-between">
+            <label className="block text-gray-700 text-sm font-bold" htmlFor="remember">
+              <input className="mr-2 leading-tight" type="checkbox" id="remember" />
+              <span className="text-sm">
+                Remember me
+              </span>
+            </label>
+          </div>
+          <button className="w-full bg-cyan-400 hover:bg-cyan-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Login
+          </button>
         </div>
-        <div className="w-full h-full p-16">
+        <div className="w-full h-full py-16">
           <img src="src\assets\images\login-mobile-testing.svg" alt="Team development image" />
         </div>
       </div>
-    </div>
+    </div >
   );
 };
