bb5eae1b13b8115d6bb4df82a33ab8a558954423
Luka Blagic <blagicluka666@gmail.com>
2024-04-03 22:22:13 +0200

    Design search and navbar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useState } from "react";
 
 export const Search = () => {
+  const [searchTerm, setSearchTerm] = useState();
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className="align-center flex h-14 w-full items-center justify-start">
+      <div className="relative w-1/6 w-full flex-row"></div>
+      <div className="px-auto relative flex w-full flex-row justify-start">
+        <div className="relative w-1/6 w-full flex-row"></div>
+        <input
+          type="text"
+          onChange={handleSearchChange}
+          className="w-1/2 rounded-lg border-2 border-blue-400 py-2 pl-4 pr-10 text-sm text-black focus:border-blue-500 focus:outline-none"
+          placeholder="Search Acme Inc"
+        />
+        <button
+          className="absolute right-2 top-0 mr-2 mt-2"
+          onClick={() => console.log(`Searching for: ${searchTerm}`)}
+        ></button>
+      </div>
+    </div>
+  );
+};
--- src/components/action-sidebar/ActionSidebar.tsx
+++ src/components/action-sidebar/ActionSidebar.tsx
@@ -7,15 +7,17 @@ export const ActionSidebar = () => {
     { name: "Jane Smith", role: "Designer" },
     { name: "Bob Johnson", role: "Product Manager" },
     { name: "Alice Williams", role: "Developer" },
-    // Add more team members as needed
   ];
 
   return (
     <div className="my-4  grid grid-rows-1">
       <div className=" flex flex-col items-center justify-center gap-4">
         <div className="vertical-center flex flex flex-col items-center items-center justify-center gap-4 text-center">
+          <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gray-300 text-xl font-bold text-black hover:bg-blue-700">
+            +
+          </div>
           {teamMembers.map((member, index) => (
-            <ActiveIcon isActive={true} position={"right"}>
+            <ActiveIcon key={index} isActive={true} position={"right"}>
               <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-blue-500 text-xl font-bold hover:bg-blue-700">
                 {member.name[0]}
               </div>
