9ca64becc9c50e4ecb92c50e9864f52641871434
Luka Blagic <blagicluka666@gmail.com>
2024-04-04 21:58:48 +0200

    Git add editor to app

--- src/components/mail/Mail.tsx
+++ src/components/mail/Mail.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { MailSidebar } from "./MailSidebar";
+import Editor from "../editor/Editor";
 
 export const Mail = () => {
   return (
     <div className="flex h-full flex-row overflow-hidden">
       <MailSidebar />
       <div className="h-full grow w-2/3 bg-white">
-        Main content
+      <Editor/>
       </div>
     </div>
   );
