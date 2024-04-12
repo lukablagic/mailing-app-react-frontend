f878bc871c2bcce2ce4133257ed70dbcc9dacdba
Luka Blagic <blagicluka666@gmail.com>
2024-04-12 23:11:54 +0200

    Add folder navigation

--- src/components/mail/MailFolders.tsx
+++ src/components/mail/MailFolders.tsx
@@ -2,60 +2,76 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import ArrorDown from "../../assets/react-icons/ArrowDown";
 import { AuthContext } from "../../utility/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useNavigation } from "../../hooks/Navigation";
 
 interface Folder {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 interface FoldersResponse {
-    folders: Folder[];
+  folders: Folder[];
 }
 
 export const MailFolders = () => {
+  const [selectedFolder, setSelectedFolder] = useState({
+    name: "Inbox",
+    id: 1,
+  });
+  const [folders, setFolders] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const { token } = useContext(AuthContext);
+  const { navigateApp } = useNavigation();
 
-    const [selectedFolder, setSelectedFolder] = useState({name: "Inbox",id: 1,});
-    const [folders, setFolders] = useState([]);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const {token} = useContext(AuthContext);
-
-    useEffect(() => {
-        axios.get<FoldersResponse>("http://localhost/api/folders/all", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        
-        }).then((response) => {
+  useEffect(() => {
+    axios
+      .get<FoldersResponse>("http://localhost/api/folders/all", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
         if (response.status === 200) {
-            setFolders(response.data.folders);
-        }});
-    }, []);
+          setFolders(response.data.folders);
+        }
+      });
+  }, []);
 
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
-    };
-
-    return (
-        <div>
-            <h1 className="mx-2 text-xl flex flex-row gap-2 items-center font-bold text-white cursor-pointer" onClick={toggleDropdown}>
-                {selectedFolder.name} <ArrorDown/>
-            </h1>
-            {dropdownOpen === true && (
-                <div className="absolute bg-blue-950 text-white w-48">
-                    {folders.map((folder: Folder) => (
-                        <div
-                            key={folder.id}
-                            className="p-2 hover:bg-gray-500 hover:text-white" 
-                            onClick={() => {
-                                setSelectedFolder(folder);
-                                setDropdownOpen(false);
-                            }}
-                        >
-                            {folder.name}
-                        </div>
-                    ))}
-                </div>
-            )}
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  
+  const handleSelectFolder = (folder: Folder) => {
+    setSelectedFolder(folder);
+    navigateApp('mail', folder.name.toLowerCase());
+  };
+  
+  return (
+    <div>
+      <h1
+        className="mx-2 flex cursor-pointer flex-row items-center gap-2 text-xl font-bold text-white"
+        onClick={toggleDropdown}
+      >
+        {selectedFolder.name} <ArrorDown />
+      </h1>
+      {dropdownOpen === true && (
+        <div className="absolute w-48 bg-blue-950 text-white">
+          {folders.map((folder: Folder) => (
+            <div
+              key={folder.id}
+              className="p-2 text-white hover:bg-gray-500"
+              onClick={() => {
+                handleSelectFolder(folder);
+                setDropdownOpen(false);
+              }}
+            >
+              {folder.name}
+              
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
