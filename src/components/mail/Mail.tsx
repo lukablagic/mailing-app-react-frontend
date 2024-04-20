c84c9a82108c0a4fb38db5f5e51c29af7e1a5e3d
Luka <blagicluka666@gmail.com>
2024-04-20 15:31:48 +0200

    Implement tabs in view

--- src/components/mail/Mail.tsx
+++ src/components/mail/Mail.tsx
@@ -1,14 +1,29 @@
-import { MailEditor } from "../editor/MailEditor";
 import { MailContent } from "../mail-content/MailContent";
 import { MailSidebar } from "../mail-sidebar/MailSidebar";
+import Tabs from "../tabs/Tabs";
 
 export const Mail = () => {
+
+  const tabData = [
+    {
+      id: 'INDEX',
+      title: 'INDEX',
+      content: <MailContent />
+    },
+  ]
+
   return (
     <div className="flex h-full flex-row overflow-hidden">
       <MailSidebar />
-      <div className="h-full w-2/3 grow bg-white overflow-auto">
-        <MailContent />
-      </div>
+      <Tabs>
+        <Tabs.Titles items={tabData.map(({ id, title }) => ({ id, title }))} />
+        <Tabs.Contents
+          items={tabData.map(({ id, content }) => ({
+            id,
+            content: <div className='tab-content'>{content}</div>,
+          }))}
+        />
+      </Tabs>
     </div>
   );
 };
---
+++ src/components/tabs/Tabs.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import TabsProvider, { useTabsContext } from '../../utility/contexts/TabsContext';
+import './assets/styles.css';
+
+type TabTitlesProps = {
+    items: {
+        id: string
+        title: string
+    }[]
+}
+
+type TabContentProps = {
+    items: {
+        id: string
+        content: React.ReactNode
+    }[]
+}
+
+type TabsComposition = {
+    Titles: (props: TabTitlesProps) => React.ReactNode
+    Contents: (props: TabContentProps) => React.ReactNode
+}
+
+type TabsProps = {
+    children: React.ReactNode
+}
+
+type TabsWrapper = (props: TabsProps) => React.ReactNode
+
+const Tabs: TabsWrapper & TabsComposition = ({ children }) => {
+    return <div className='tabs-wrapper'><TabsProvider>{children}</TabsProvider></div>
+}
+
+Tabs.Titles = ({ items }) => {
+    const { currentIndex, setCurrentIndex } = useTabsContext()
+    return (
+        <div role="tab-list" className='tab-list' >
+            {items.map(({ id, title }, index) => (
+                <button
+                    className={`tab-list-item ${currentIndex === index ? 'selected-tab-item' : ''}`}
+                    key={id}
+                    id={`tab-control-${id}`}
+                    role="tab"
+                    aria-controls={`tab-content-${id}`}
+                    aria-selected={currentIndex === index}
+                    onClick={() => {
+                        setCurrentIndex(index)
+                    }}
+                >
+                    {title}
+                </button>
+            ))}
+        </div>
+    )
+}
+
+Tabs.Contents = ({ items }) => {
+    const { currentIndex } = useTabsContext()
+    const { id, content } = items[currentIndex]
+
+    return (
+        <div
+            className='tab-content'
+            key={id}
+            id={`tab-content-${id}`}
+            role="tabpanel"
+            aria-labelledby={`tab-control-${id}`}
+        >
+            {content}
+        </div>
+    );
+}
+
+export default Tabs
