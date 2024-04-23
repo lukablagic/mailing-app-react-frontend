241111c87d78924fd707f639e7847075de11ff3a
Luka <blagicluka666@gmail.com>
2024-04-24 00:10:43 +0200

    Fix toast design

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useRef } from 'react';
 import './assets/styles.css';
+
 export const Toast = ({ message, type }) => {
-    return (
-        <div className={`toast toast-${type}`}>
-            {message}
-        </div>
-    );
-}
+    const toastRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    return () => {
+      if (toastRef.current) {
+        toastRef.current.classList.add('fade-out');
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={toastRef} className={`toast toast-${type}`}>
+      {message}
+    </div>
+  );
+};
--- src/utility/contexts/ToastContext.tsx
+++ src/utility/contexts/ToastContext.tsx
@@ -1,23 +1,26 @@
-import React, { createContext, useState, useContext } from "react";
+import  { createContext, useState } from "react";
 import { Toast } from "../../components/toast/Toast";
 
 const ToastContext = createContext(null);
 
 function ToastProvider({ children }) {
-  const [toastData, setToastData] = useState(null);
+  const [toasts, setToasts] = useState([]);
 
-  const showToast = (type, message, position = "top-center") => {
-    setToastData({ type, message, position });
-  };
+  const showToast = (type, message, position = "top-center", duration = 1000) => {
+    const id = Math.random().toString(36).slice(4, 9); 
+    setToasts((oldToasts) => [...oldToasts, { id, type, message, position }]);
 
-  const hideToast = () => {
-    setToastData(null);
+    setTimeout(() => {
+      setToasts((oldToasts) => oldToasts.filter((toast) => toast.id !== id));
+    }, duration);
   };
 
   return (
-    <ToastContext.Provider value={{ showToast, hideToast }}>
+    <ToastContext.Provider value={{ showToast }}>
       {children}
-      {toastData && <Toast {...toastData} />}
+      {toasts.map((toast) => (
+        <Toast key={toast.id} {...toast} />
+      ))}
     </ToastContext.Provider>
   );
 }
