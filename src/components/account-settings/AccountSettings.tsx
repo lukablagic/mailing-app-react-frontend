2c5a767790c98c291644a41ca8cb650d83f448cf
Luka <blagicluka666@gmail.com>
2024-05-09 23:52:54 +0200

    Implement update account settings

--- src/components/account-settings/AccountSettings.tsx
+++ src/components/account-settings/AccountSettings.tsx
@@ -20,8 +20,18 @@ interface Credentials {
 
 export const AccountSettings = () => {
 
-    const [credentials, setCredentials] = useState<Credentials>();
-    const { auth }                      = useContext(AuthContext);
+    const { auth }                                = useContext(AuthContext);
+    const [credentials, setCredentials]           = useState<Credentials>();
+    const [showSmtpPassword, setShowSmtpPassword] = useState(false);
+    const [showImapPassword, setShowImapPassword] = useState(false);
+
+    const toggleSmtpPasswordVisibility = () => {
+        setShowSmtpPassword(!showSmtpPassword);
+    };
+
+    const toggleImapPasswordVisibility = () => {
+        setShowImapPassword(!showImapPassword);
+    };
 
     useEffect(() => {
         axios.get(BASE_URL + '/teams/team-crendentials',
@@ -47,7 +57,22 @@ export const AccountSettings = () => {
     };
 
     const handleUpdate = () => {
-        // Send a request to update the settings
+        axios.put(BASE_URL + '/teams/team-crendentials',
+            {
+                credentials
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            }
+        ).then((response) => {
+            if (response.status === 200) {
+                console.log('Credentials updated')
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
     };
 
     return (
@@ -59,12 +84,14 @@ export const AccountSettings = () => {
                 <label>Port: <input type="number" name="imap_port" className='settings-input' value={credentials?.imap_port} onChange={handleInputChange} /></label>
                 <label>Protocol: <input type="text" name="protocol" className='settings-input' value={credentials?.protocol} onChange={handleInputChange} /></label>
                 <label>Email: <input type="email" name="email" className='settings-input' value={credentials?.email} onChange={handleInputChange} /></label>
-                <label>Password: <input type="password" name="imap_password" className='settings-input' value={credentials?.imap_password} onChange={handleInputChange} /></label>
+                <label>Password: <input type={showImapPassword ? "text" : "password"} name="imap_password" className='settings-input' value={credentials?.imap_password} onChange={handleInputChange} /></label>
+                <label>Show Password: <input type="checkbox" onChange={toggleImapPasswordVisibility} /></label>
             </div>
             <div>
                 <h2 className="settings-content-title">SMTP</h2>
                 <label>Server: <input type="text" name="smtp_server" className='settings-input' value={credentials?.smtp_server} onChange={handleInputChange} /></label>
-                <label>Password: <input type="password" name="smtp_password" className='settings-input' value={credentials?.smtp_password} onChange={handleInputChange} /></label>
+                <label>Password: <input type={showSmtpPassword ? "text" : "password"} name="smtp_password" className='settings-input' value={credentials?.smtp_password} onChange={handleInputChange} /></label>
+                <label>Show Password: <input type="checkbox" onChange={toggleSmtpPasswordVisibility} /></label>
             </div>
             <button className='plus-button' onClick={handleUpdate}>Update</button>
         </div>
